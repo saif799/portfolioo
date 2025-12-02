d3edea76e94e241eeb457ad2714b0e0f65df98db
saif799 <faycalbakziz@gmail.com>
2025-12-02 21:29:09 +0100

    fix

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -3,10 +3,10 @@ import Work from "@/components/work/Work";
 import Home from "@/components/home/Home";
 
 export default function Page() {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Home isHome={false} />
-            <Work seeAll={true} />
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Home />
+      <Work seeAll={true} />
+    </Suspense>
+  );
 }
